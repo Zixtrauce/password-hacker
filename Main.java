94f4a2890b48fe0eb787554046fe02aa763e8a98
zixtrauce <sshar564@uwo.ca>
2023-07-23 21:22:30 -0700

    Implement basic IO connection to server

--- Main.java
+++ Main.java
@@ -1,7 +1,43 @@
 package hacker;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        //initialize required variables
+        String ip_address;
+        int port;
+        String message_for_sending;
+
+        //process arguments
+        ip_address = args[0];
+        port = Integer.parseInt(args[1]);
+        message_for_sending = args[2];
+
+        try (
+
+            //create new socket and datastream to recieve input
+            Socket socket = new Socket(ip_address, port);
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+
+          ) {
+            //send message from commandline argument to server
+            output.writeUTF(message_for_sending);
+            //read reply from server
+            String receivedMsg = input.readUTF();
+
+            //output received message
+            System.out.println(receivedMsg);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
